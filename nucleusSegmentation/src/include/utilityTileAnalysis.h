e13ea5e64240678a42ba103fc00abb405cac0809
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2016-12-21 16:35:55 -0500

    time
    Tracking how long it takes to perform the segmentation.

--- nucleusSegmentation/src/include/utilityTileAnalysis.h
+++ nucleusSegmentation/src/include/utilityTileAnalysis.h
@@ -29,6 +29,8 @@
 #include "utilityScalarImage.h"
 #include "utilityIO.h"
 
+#include "time.h"
+
 namespace ImagenomicAnalytics
 {
   namespace TileAnalysis
@@ -449,12 +451,17 @@ namespace ImagenomicAnalytics
           std::cout<<"before CV\n"<<std::flush;
 
           //int numiter = 100;
+          time_t start, end;
+          time(&start);
           CSFLSLocalChanVeseSegmentor2D< itkFloatImageType::PixelType > cv;
           cv.setImage(hemaFloat);
           cv.setMask( nucleusBinaryMask );
           cv.setNumIter(levelsetNumberOfIteration);
           cv.setCurvatureWeight(curvatureWeight);
           cv.doSegmenation();
+          time(&end);
+          double dif = difftime(end, start);
+          printf("Elasped time is %.2lf seconds.\n", dif);
 
           std::cout<<"after CV\n"<<std::flush;
 
@@ -521,12 +528,13 @@ namespace ImagenomicAnalytics
             }
         }
 
+      std::cout << "before ConnectedComponent\n" << std::flush;
       typedef itk::ConnectedComponentImageFilter <itkUCharImageType, itkUShortImageType > ConnectedComponentImageFilterType;
       ConnectedComponentImageFilterType::Pointer connected = ConnectedComponentImageFilterType::New ();
       connected->SetInput(nucleusBinaryMask);
       connected->Update();
-
       outputLabelImageUShort = connected->GetOutput();
+      std::cout << "after ConnectedComponent\n" << std::flush;
 
       return nucleusBinaryMask;
     }
@@ -588,12 +596,17 @@ namespace ImagenomicAnalytics
           std::cout<<"before CV\n"<<std::flush;
 
           //int numiter = 100;
+          time_t start, end;
+          time(&start);
           CSFLSLocalChanVeseSegmentor2D< itkFloatImageType::PixelType > cv;
           cv.setImage(hemaFloat);
           cv.setMask( nucleusBinaryMask );
           cv.setNumIter(levelsetNumberOfIteration);
           cv.setCurvatureWeight(curvatureWeight);
           cv.doSegmenation();
+          time(&end);
+          double dif = difftime(end, start);
+          printf("Elasped time is %.2lf seconds.\n", dif);
 
           std::cout<<"after CV\n"<<std::flush;
 

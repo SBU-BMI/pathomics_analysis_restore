b952267d0f8d13616978fa0ce6c1617dd6159328
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-01-26 10:59:27 -0500

    Update test.sh

--- nucleusSegmentation/test_segmentation/test.sh
+++ nucleusSegmentation/test_segmentation/test.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 docker_name="$USER-test_segmentation"
-cd ../script
+exec_id="20170126105652"
+input_file="TCGA-CS-4938-01Z-00-DX1_12560_47520_500_500_LGG.png"
+output_file="$HOME/test_out.zip"
 
-# SEGMENT IMAGE
-exec_id="20170112055937"
-input_file="../test_segmentation/TCGA-CS-4938-01Z-00-DX1_12560_47520_500_500_LGG.png"
-
-# One-liner:
-#./run_docker_segment.py segment $docker_name $input_file $HOME/test_out.zip -t img -s 12560,47520 -b 500,500 -d 500,500 -a $exec_id -c TCGA-CS-4938-01Z-00-DX1 -p TCGA-CS-4938
-
-# Line-breaks, easier to read:
-./run_docker_segment.py \
+# Segment image
+python ../script/run_docker_segment.py \
 segment \
 $docker_name \
 $input_file \
-$HOME/test_out.zip \
+$output_file \
 -t img \
 -j Y \
 -s 12560,47520 \

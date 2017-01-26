237760e9a5acc5643242bd5ec9d13ef9d985f3bb
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-01-26 10:54:21 -0500

    Update init.sh

--- nucleusSegmentation/test_segmentation/init.sh
+++ nucleusSegmentation/test_segmentation/init.sh
@@ -2,10 +2,6 @@
 
 docker build -t sbubmi/test_segmentation .
 
-git clone https://github.com/SBU-BMI/pathomics_analysis.git
-
-cd pathomics_analysis/nucleusSegmentation/script
-
 docker_image="sbubmi/test_segmentation"
 docker_name="$USER-test_segmentation"
-./run_docker_segment.py start $docker_name $docker_image
+python ../script/run_docker_segment.py start $docker_name $docker_image

7a1a9760e29e20616d32e74e2264076acb3fc9bc
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-02-08 13:16:14 -0500

    computeFeatures

--- nucleusSegmentation/src/include/MultipleObjectFeatureAnalysisFilter.h
+++ nucleusSegmentation/src/include/MultipleObjectFeatureAnalysisFilter.h
@@ -10,105 +10,127 @@
 #include "itkTypedefs.h"
 #include "SingleObjectFeatureAnalysisFilter.h"
 
-namespace ImagenomicAnalytics
-{
-  class MultipleObjectFeatureAnalysisFilter
-  {
-  public:
-    typedef MultipleObjectFeatureAnalysisFilter Self;
+#include "utilityFeaturesIO.h"
 
-    ////////////////////////////////////////////////////////////////////////////////
-    /// ctor
-    MultipleObjectFeatureAnalysisFilter();
-    MultipleObjectFeatureAnalysisFilter(std::ostream& outputStream);
-    ~MultipleObjectFeatureAnalysisFilter() {}
-    /// ctor, end
-    ////////////////////////////////////////////////////////////////////////////////
+namespace ImagenomicAnalytics {
+    class MultipleObjectFeatureAnalysisFilter {
+    public:
+        typedef MultipleObjectFeatureAnalysisFilter Self;
+
+        ////////////////////////////////////////////////////////////////////////////////
+        /// ctor
+        MultipleObjectFeatureAnalysisFilter();
+
+        MultipleObjectFeatureAnalysisFilter(std::ostream &outputStream);
+
+        ~MultipleObjectFeatureAnalysisFilter() {}
+        /// ctor, end
+        ////////////////////////////////////////////////////////////////////////////////
 
 
-    ////////////////////////////////////////////////////////////////////////////////
-    /// public fn
+        ////////////////////////////////////////////////////////////////////////////////
+        /// public fn
 
-    ///--------------------------------------------------------------------------------
-    /// Input: Note that we Assume the spacing of both m_RGBImage and
-    /// m_binaryObjectMask are correctly set to mpp
-    void setInputRGBImage(itkRGBImageType::Pointer inputRGBImage) {m_RGBImage = inputRGBImage;}
-    void setObjectBinaryMask(itkBinaryMaskImageType::Pointer binaryObjectMask) {
-			m_binaryObjectMask = binaryObjectMask;
+        ///--------------------------------------------------------------------------------
+        /// Input: Note that we Assume the spacing of both m_RGBImage and
+        /// m_binaryObjectMask are correctly set to mpp
+        void setInputRGBImage(itkRGBImageType::Pointer inputRGBImage) { m_RGBImage = inputRGBImage; }
+
+        void setObjectBinaryMask(itkBinaryMaskImageType::Pointer binaryObjectMask) {
+            m_binaryObjectMask = binaryObjectMask;
             m_labelComputed = 0;
-	}
-    void setTopLeft(int64_t x, int64_t y) {m_TopLeftX = x; m_TopLeftY = y;}
+        }
 
-	void setObjectLabeledMask(itkLabelImageType::Pointer labeledMask) {
+        void setTopLeft(int64_t x, int64_t y) {
+            m_TopLeftX = x;
+            m_TopLeftY = y;
+        }
+
+        void setObjectLabeledMask(itkLabelImageType::Pointer labeledMask) {
             m_objectLabelImage = labeledMask;
             m_labelComputed = 1;
-    }
+        }
 
-	void setFeatureNames() {
-         ImagenomicAnalytics::SingleObjectFeatureAnalysisFilter tmp;
-         featureNames = tmp.getFeatureNames();
-    }
+        void setFeatureNames() {
+            ImagenomicAnalytics::SingleObjectFeatureAnalysisFilter tmp;
+            featureNames = tmp.getFeatureNames();
+            this->numberOfFeatureNoPolygon = static_cast<int>(this->featureNames.size());
+            std::cout << this->numberOfFeatureNoPolygon << std::endl;
+        }
 
-	std::vector <std::string> getFeatureNames() {
+        std::vector<std::string> getFeatureNames() {
             return featureNames;
-    }
+        }
 
-    std::vector< std::vector<FeatureValueType> > getFeatures();
+        std::vector<std::vector<FeatureValueType> > getFeatures();
 
-    void update();
-    ///================================================================================
-
-
-
-    ///--------------------------------------------------------------------------------
-    /// Output
-    void outputFeaturesToFile(std::string outputFeaturesFileName);
-    void outputFeaturesToConsole();
-    ///================================================================================
-
-    /// public fn, end
-    ////////////////////////////////////////////////////////////////////////////////
+        void update();
+        ///================================================================================
 
 
-  private:
-    std::ostream& m_outputStream;
+        ///--------------------------------------------------------------------------------
+        /// Output
+        void outputFeaturesToFile(std::string outputFeaturesFileName);
 
-    ////////////////////////////////////////////////////////////////////////////////
-    /// private data
+        void outputFeaturesToConsole();
+        ///================================================================================
 
-    ///--------------------------------------------------------------------------------
-    /// Input data
-    itkRGBImageType::Pointer m_RGBImage;
-    itkBinaryMaskImageType::Pointer m_binaryObjectMask;
-    int64_t m_TopLeftX; // the x of the top left cornor of this tile in the WSI, in index (pixel) space
-    int64_t m_TopLeftY;
-    ///================================================================================
-    
-	int m_labelComputed;
+        ///--------------------------------------------------------------------------------
+        /// Output 2
+        void outputFeaturesCSV(std::ofstream &outputFeatureFile, std::vector<std::vector<FeatureValueType> > features) {
+            printFeatureNames(featureNames, outputFeatureFile);
+            printFeatures(features, outputFeatureFile, numberOfFeatureNoPolygon);
 
-    std::vector <std::string> featureNames;
+        }
+        ///================================================================================
 
-    ///--------------------------------------------------------------------------------
-    /// Computed data
-    itkLabelImageType::Pointer m_objectLabelImage;
-    std::vector<std::string> m_featureNames;
-    std::vector< std::vector<FeatureValueType> > m_featuresOfAllObjects;
-
-    int64_t m_totalNumberOfConnectedComponents;
-    ///================================================================================
-
-    bool m_allDone;
-    /// private data, end
-    ////////////////////////////////////////////////////////////////////////////////
+        /// public fn, end
+        ////////////////////////////////////////////////////////////////////////////////
 
 
-    ////////////////////////////////////////////////////////////////////////////////
-    /// private fn
-    void _init();
-    void _computeFeaturesForAllObjects();
-    /// private fn
-    ////////////////////////////////////////////////////////////////////////////////
-  };
+    private:
+        std::ostream &m_outputStream;
+
+        ////////////////////////////////////////////////////////////////////////////////
+        /// private data
+
+        ///--------------------------------------------------------------------------------
+        /// Input data
+        itkRGBImageType::Pointer m_RGBImage;
+        itkBinaryMaskImageType::Pointer m_binaryObjectMask;
+        int64_t m_TopLeftX; // the x of the top left corner of this tile in the WSI, in index (pixel) space
+        int64_t m_TopLeftY;
+        ///================================================================================
+
+        int m_labelComputed;
+
+        std::vector<std::string> featureNames;
+
+        ///--------------------------------------------------------------------------------
+        /// Computed data
+        itkLabelImageType::Pointer m_objectLabelImage;
+        std::vector<std::string> m_featureNames;
+        std::vector<std::vector<FeatureValueType> > m_featuresOfAllObjects;
+
+        int64_t m_totalNumberOfConnectedComponents;
+        ///================================================================================
+
+        // Output
+        int numberOfFeatureNoPolygon;
+
+        bool m_allDone;
+        /// private data, end
+        ////////////////////////////////////////////////////////////////////////////////
+
+
+        ////////////////////////////////////////////////////////////////////////////////
+        /// private fn
+        void _init();
+
+        void _computeFeaturesForAllObjects();
+        /// private fn
+        ////////////////////////////////////////////////////////////////////////////////
+    };
 
 }// namespace gth818n
 

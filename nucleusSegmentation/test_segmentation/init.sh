8d2b01f795f7a1780411ec378babf2e5ad70b078
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-02-09 15:47:09 -0500

    name

--- nucleusSegmentation/test_segmentation/init.sh
+++ nucleusSegmentation/test_segmentation/init.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#docker_image="sbubmi/test_segmentation"
+#docker_name="$USER-test_segmentation"
+docker_image="sbubmi/quip-jobs"
+docker_name="quip-jobs"
+
 # Build image from dockerfile
-docker build -t sbubmi/test_segmentation .
+docker build -t $docker_image .
 
 # Start container from image
-docker_image="sbubmi/test_segmentation"
-docker_name="$USER-test_segmentation"
 python ../script/run_docker_segment.py start $docker_name $docker_image
